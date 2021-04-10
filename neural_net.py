5e1c58d1fd4c412b961fce245cc9bbbc15413c96
hintonlever <b.hintonlever@gmail.com>
2021-04-10 20:37:07 +1000

    Update neural_net.py
    simple feed forward

--- neural_net.py
+++ neural_net.py
@@ -4,9 +4,23 @@ Created on Sun Apr  4 17:44:35 2021
 
 @author: Benji
 """
+
+# Helpful websites 
+# https://towardsdatascience.com/everything-you-need-to-know-about-neural-networks-and-backpropagation-machine-learning-made-easy-e5285bc2be3a
+
 # Requires numpy for matrix calcs
 
 import numpy as np
+
+import random
+
+# Activation functions
+def sigmoid(x):
+   return 1 / (1 + np.exp(-x))
+
+
+
+# Simple neural network with customisable node lengths and 1 hidden layer, sigmoid activations
 
 class Neural_Net():
     def __init__(self,input_nodes_count,hidden_nodes_count,output_nodes_count):
@@ -14,14 +28,33 @@ class Neural_Net():
         self.hidden_nodes_count = hidden_nodes_count
         self.output_nodes_count = output_nodes_count
         
-        # Create and randomise weight matrices (includes bias vector)
+        # Create and randomise weight matrices (includes + 1 for bias vector)
 
-        self.weights_1 = np.random.rand(self.hidden_nodes_count,self.input_nodes_count + 1) 
-        self.weights_2 = np.random.rand(self.hidden_nodes_count, self.hidden_nodes_count + 1)
-        self.weights_3 = np.random.rand(self.output_nodes_count, self.hidden_nodes_count + 1)
+        self.weights_1 = np.random.rand(self.hidden_nodes_count, self.input_nodes_count  + 1) 
+        self.weights_2 = np.random.rand(self.output_nodes_count, self.hidden_nodes_count + 1)
+        # self.weights_3 = np.random.rand(self.output_nodes_count, self.hidden_nodes_count)
    
+        self.bias_1 = random.random()
+        self.bias_2 = random.random()
+        # self.bias_3 = np.random.rand(self.output_nodes_count, self.hidden_nodes_count + 1)
     
-   def predict(self,input_vector):
+    def predict(self, input_vector):
         # Feed forward inputs through neural network
         
-        return(1)
+        # Add bias to the input vector
+        input_w_bias = np.append(input_vector,self.bias_1)
+        
+        # Use dot product to get hidden layer values
+        out_1 = sigmoid(np.dot(self.weights_1,input_w_bias))
+        
+        # Add bais to the second layer
+        out_1_w_bias = np.append(out_1,self.bias_2)
+        
+        # Apply activation function to output nodes
+        out_2 = sigmoid(np.dot(self.weights_2,out_1_w_bias ))
+
+        return(out_2)
+    
+    
+    
+
